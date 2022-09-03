6b81a23fb7d910b1852a5160f9bec53c82ad58e0
Pedro Henrique <pedrohenrique053010@gmail.com>
2022-09-02 22:35:37 -0300

    resolução do exercicio

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Mongaguá");
+Thread.Sleep(1000);
+Console.WriteLine("Itanhaém");
+Thread.Sleep(1000);
+Console.WriteLine("Peruíbe");
+Thread.Sleep(1000);
+Console.WriteLine("Praia Grande");
+
